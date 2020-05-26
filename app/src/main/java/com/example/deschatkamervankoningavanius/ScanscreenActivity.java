ba2c5ce69a6f301630d83a3f8e2dbcfd37e8bfad
Jan Kees <jc.broekhuizen@student.avans.nl>
2020-05-26 15:30:48 +0200

    [Init] : Added the scanscreen with popup

--- app/src/main/java/com/example/deschatkamervankoningavanius/ScanscreenActivity.java
+++ app/src/main/java/com/example/deschatkamervankoningavanius/ScanscreenActivity.java
@@ -1,4 +1,25 @@
 package com.example.deschatkamervankoningavanius;
 
-public class ScanscreenActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScanscreenActivity extends AppCompatActivity {
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanscreen);
+
+    }
+
+    public void onIconQuestionClicked(View v) {
+        Intent intent = new Intent(ScanscreenActivity.this, Pop.class);
+        ScanscreenActivity.this.startActivity(intent);
+    }
 }
