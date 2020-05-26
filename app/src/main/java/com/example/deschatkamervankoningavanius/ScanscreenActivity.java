dad0e99aaffd460f915d757c1ad59978d3ceabc8
Dave-Avans <dl.visser@student.avans.nl>
2020-05-26 16:06:55 +0200

    [Edit] : Succesfully merged scanscreens with development.

--- app/src/main/java/com/example/deschatkamervankoningavanius/ScanscreenActivity.java
+++ app/src/main/java/com/example/deschatkamervankoningavanius/ScanscreenActivity.java
@@ -3,13 +3,12 @@ package com.example.deschatkamervankoningavanius;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class ScanscreenActivity extends AppCompatActivity {
-    ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,5 +20,12 @@ public class ScanscreenActivity extends AppCompatActivity {
     public void onIconQuestionClicked(View v) {
         Intent intent = new Intent(ScanscreenActivity.this, Pop.class);
         ScanscreenActivity.this.startActivity(intent);
+    }
+
+
+    //TODO: REMOVE after QR and NFC works.
+    public void onButtonQRPressed(View v){
+        Intent intent = new Intent(ScanscreenActivity.this,NavActivityFragmentBase.class);
+        startActivity(intent);
     }
 }
