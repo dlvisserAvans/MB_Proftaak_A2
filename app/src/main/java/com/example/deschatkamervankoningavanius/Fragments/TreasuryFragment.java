7cedca64aa246e9d839b96afad93aa6d226e931d
Jan Kees <jc.broekhuizen@student.avans.nl>
2020-05-28 11:59:06 +0200

    [Add] function button treasuryScreen

--- app/src/main/java/com/example/deschatkamervankoningavanius/Fragments/TreasuryFragment.java
+++ app/src/main/java/com/example/deschatkamervankoningavanius/Fragments/TreasuryFragment.java
@@ -7,11 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.deschatkamervankoningavanius.Data.Quest;
 import com.example.deschatkamervankoningavanius.R;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
@@ -24,19 +27,42 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TreasuryFragment extends Fragment {
     final String TAG = TreasuryFragment.class.getName();
 
     private MqttAndroidClient client;
-    private Button button;
+    private String topic = "group/A2/word";
+    private String payload = "aaaa";
+    private byte[] encodedPayload = new byte[0];
+    private List<Quest> questions;
+    private TextView textView;
+    private Button buttonCheck;
+    private List<String> collectedLetters;
+    private String letters = "";
+    private int clickamount;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        String clientId = MqttClient.generateClientId();
+        View view = inflater.inflate(R.layout.fragment_treasury, container,false);
+        this.clickamount = 0;
+        this.questions = new ArrayList<>();
+        this.collectedLetters = new ArrayList();
+        for (int i = 0; i < 6; i++){
+            this.collectedLetters.add(".");
+            letters += ". ";
+        }
 
-        this.client = new MqttAndroidClient(this.getContext(), "tcp://maxwell.bps-software.nl:1883", clientId);
+        this.textView = (TextView) view.findViewById(R.id.tvCollectedLetters);
+        this.textView.setText(letters);
+
+
+        String clientId = MqttClient.generateClientId();
+        this.client = new MqttAndroidClient(getActivity().getApplicationContext(), "tcp://maxwell.bps-software.nl:1883", clientId);
 
         try {
             MqttConnectOptions options = new MqttConnectOptions();
@@ -59,28 +85,49 @@ public class TreasuryFragment extends Fragment {
             e.printStackTrace();
         }
 
-        this.button = (Button) container.findViewById(R.id.buttonVerify);
-        this.button.setOnClickListener(new View.OnClickListener() {
+        this.buttonCheck = (Button) view.findViewById(R.id.buttonVerify);
+        buttonCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String payload = "HELP";
-                String topic = "group/A2/word";
-
-                byte[] encodedPayload = new byte[0];
-                try {
-                    encodedPayload = payload.getBytes("UTF-8");
-                    MqttMessage message = new MqttMessage(encodedPayload);
-                    client.publish(topic, message);
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                } catch (MqttPersistenceException e) {
-                    e.printStackTrace();
-                } catch (MqttException e) {
-                    e.printStackTrace();
-                }
+               onButtonCheckClicked(v);
             }
         });
 
-        return inflater.inflate(R.layout.fragment_treasury, container, false);
+        return view;
+    }
+
+    public void onButtonCheckClicked(View view){
+        //TODO: Integrate function when Checkmenu is finished
+//        Intent intent = new Intent(getActivity(),SettingsMenuActivity.class);
+//        startActivity(intent);
+
+//        try {
+//            encodedPayload = payload.getBytes("UTF-8");
+//            MqttMessage message = new MqttMessage(encodedPayload);
+//            System.out.println(client);
+//            System.out.println(message.getPayload());
+//            client.publish(topic, message);
+//        } catch (UnsupportedEncodingException | MqttException e) {
+//            e.printStackTrace();
+//        }
+        if (clickamount > 5){
+            clickamount = 0;
+        } else {
+            clickamount++;
+        }
+        changeLetterCollection("a", clickamount-1);
+
+        Toast.makeText(getActivity(), "Check",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void changeLetterCollection(String letter, int position){
+        this.letters = "";
+        this.collectedLetters.set(position, letter);
+        for (String string : collectedLetters){
+            this.letters += string + " ";
+        }
+        this.textView.setText(this.letters);
     }
 }
+
