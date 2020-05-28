7e4a018efa2d5e1ca0761d3395af8f50d3aeef85
Jan Kees <jc.broekhuizen@student.avans.nl>
2020-05-28 10:21:21 +0200

    [Add] buttonfunction treasuryscreen

--- app/src/main/java/com/example/deschatkamervankoningavanius/Fragments/TreasuryFragment.java
+++ app/src/main/java/com/example/deschatkamervankoningavanius/Fragments/TreasuryFragment.java
@@ -1,9 +1,12 @@
 package com.example.deschatkamervankoningavanius.Fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +14,73 @@ import androidx.fragment.app.Fragment;
 
 import com.example.deschatkamervankoningavanius.R;
 
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
+import java.io.UnsupportedEncodingException;
+
 public class TreasuryFragment extends Fragment {
+    final String TAG = TreasuryFragment.class.getName();
+
+    private MqttAndroidClient client;
+    private Button button;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        String clientId = MqttClient.generateClientId();
+
+        this.client = new MqttAndroidClient(this.getContext(), "tcp://maxwell.bps-software.nl:1883", clientId);
+
+        try {
+            MqttConnectOptions options = new MqttConnectOptions();
+            options.setUserName("androidTI");
+            options.setPassword("&FN+g$$Qhm7j".toCharArray());
+            IMqttToken token = client.connect(options);
+
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(TAG, "connected!");
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(TAG, "connection failed! " + exception.getMessage());
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        this.button = (Button) container.findViewById(R.id.buttonVerify);
+        this.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String payload = "HELP";
+                String topic = "group/A2/word";
+
+                byte[] encodedPayload = new byte[0];
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    MqttMessage message = new MqttMessage(encodedPayload);
+                    client.publish(topic, message);
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (MqttPersistenceException e) {
+                    e.printStackTrace();
+                } catch (MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_treasury, container, false);
     }
 }
