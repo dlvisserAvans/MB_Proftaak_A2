724d519146ffbe771e59ebb512e653e68abe3493
Rik <rik.vos01@gmail.com>
2020-05-28 12:36:37 +0200

    [Edit] Added comments

--- app/src/main/java/com/example/deschatkamervankoningavanius/QR_code_scanner.java
+++ app/src/main/java/com/example/deschatkamervankoningavanius/QR_code_scanner.java
@@ -24,10 +24,16 @@ import java.util.ArrayList;
 
 public class QR_code_scanner extends AppCompatActivity {
 
-    SurfaceView surfaceView;
-    TextView displayQRCodeValue;
+
+    //here we get the camera feed from
     private CameraSource cameraSource;
-    String qrCodeValue;
+    //here we display the camera feed
+    private SurfaceView surfaceView;
+    //here we save the string from the qr-code
+    private String qrCodeValue;
+    //here we display the qr-code value
+    private TextView displayQRCodeValue;
+    //array list with all correct values
     private ArrayList<String> correctValues;
 
     @Override
@@ -35,26 +41,36 @@ public class QR_code_scanner extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code_scanner);
 
+        //find the views and set them as attributes
         this.displayQRCodeValue = findViewById(R.id.displayQRCodeValue);
         this.surfaceView = findViewById(R.id.surfaceView);
 
+        //initialise the rest of the attributes
         this.qrCodeValue = "";
         this.correctValues = new ArrayList<>();
-        //maybe we can fill this array with a REST API
+        //todo maybe we can fill this array with a REST API
         this.correctValues.add("Avanius");
     }
 
-
+    /**
+     * this function initialises the following things
+     * -barcodeDetector
+     * -cameraSource
+     * -surfaceView
+     */
     private void initialiseDetectorsAndSources() {
+        //make a barcode detector and set its mode to qr-codes
         BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
 
+        //make a camera source and pass it the barcode detector so it knows what it is searching for
         cameraSource = new CameraSource.Builder(this, barcodeDetector)
                 .setRequestedPreviewSize(1920, 1080)
                 .setAutoFocusEnabled(true) //you should add this feature
                 .build();
 
+        //add a callback so the display is updated real time
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
@@ -77,13 +93,14 @@ public class QR_code_scanner extends AppCompatActivity {
             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
             }
 
+            //if we quit we must close the source so they don't keep existing
             @Override
             public void surfaceDestroyed(SurfaceHolder holder) {
                 cameraSource.stop();
             }
         });
 
-
+        //here we declare what we want to do if we detect something with the barcode detector
         barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
             @Override
             public void release() {
@@ -91,17 +108,22 @@ public class QR_code_scanner extends AppCompatActivity {
 
             @Override
             public void receiveDetections(Detector.Detections<Barcode> detections) {
+                //the barcode detector returns a SparseArray of barcode's
                 final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                //we check if there are any detected
                 if (barcodes.size() != 0) {
+                    //make a thread so it updates the display
                     displayQRCodeValue.post(new Runnable() {
                         @Override
                         public void run() {
+                            //we only use the first one it scans
                             qrCodeValue = barcodes.valueAt(0).displayValue;
+                            //set the value to the display in de upper left corner
                             displayQRCodeValue.setText(qrCodeValue);
 
-                            //todo check if the value is in the array with valid values
+                            //check if the value is in the array with valid values
                             if (correctValues.contains(qrCodeValue)) {
-                                //you could use this method for this
+                                //it is so we can proceed
                                 difficultySelected();
                             }
 
@@ -116,22 +138,34 @@ public class QR_code_scanner extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        //make sure there aren't unnecessary things running
         cameraSource.release();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        //because we released it we have to re-initiate everything
         initialiseDetectorsAndSources();
     }
 
+    /**
+     * with this function we start the home page
+     * we pass the barcode value to it via the intent
+     */
     private void difficultySelected() {
+        //we create a intent to go to the home page
         Intent intent = new Intent(this, NavActivityFragmentBase.class);
+        //add the value from the barcode to the intent
         intent.putExtra("Difficulty", qrCodeValue);
+        //start the intent
         startActivity(intent);
     }
 
-
+    /**
+     * onClick for the bottom button (only used for debugging)
+     * @param view
+     */
     public void onButtonChooseDifficultyPressed(View view) {
         difficultySelected();
     }
