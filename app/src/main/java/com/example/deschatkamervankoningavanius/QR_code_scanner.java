7545099a7f933f2cbb777ca7512cfcfc04a5ab3e
Rik <rik.vos01@gmail.com>
2020-05-28 10:30:27 +0200

    [Edit] transition to QR-scan activity

---
+++ app/src/main/java/com/example/deschatkamervankoningavanius/QR_code_scanner.java
@@ -0,0 +1,16 @@
+package com.example.deschatkamervankoningavanius;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+public class QR_code_scanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code_scanner);
+
+    }
+}
