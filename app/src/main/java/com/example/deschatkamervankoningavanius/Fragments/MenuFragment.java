84812b309d626ee2a8c714c844d79e3348f9d325
Dave-Avans <dl.visser@student.avans.nl>
2020-05-28 10:14:13 +0200

    [Init]/[Add] : created the menu fragment with the buttons
    Changed the button looks and functionality, minor layout fixes to treasury, buttons are noticably clickable.

--- app/src/main/java/com/example/deschatkamervankoningavanius/Fragments/MenuFragment.java
+++ app/src/main/java/com/example/deschatkamervankoningavanius/Fragments/MenuFragment.java
@@ -1,9 +1,12 @@
 package com.example.deschatkamervankoningavanius.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,7 +19,45 @@ public class MenuFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_menu,container,false);
+        Button buttonHelp = view.findViewById(R.id.btn_menu_help);
+        Button buttonSettings = view.findViewById(R.id.btn_menu_settings);
+
+        buttonHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonHelpClicked(v);
+            }
+        });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onButtonSettingsClicked(v);
+            }
+        });
+
+
+        return view;
+    }
+
+    public void onButtonSettingsClicked(View view){
+        //TODO: Integrate function when settingsmenu is finished
+//        Intent intent = new Intent(getActivity(),SettingsMenuActivity.class);
+//        startActivity(intent);
+
+        Toast.makeText(getActivity(), "Settings",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onButtonHelpClicked(View view){
+        //TODO: Integrate function when helpmenu is finished
+//        Intent intent = new Intent(getActivity(),HelpMenuActivity.class);
+//        startActivity(intent);
+
+        Toast.makeText(getActivity(), "Help",
+                Toast.LENGTH_SHORT).show();
     }
 
 }
