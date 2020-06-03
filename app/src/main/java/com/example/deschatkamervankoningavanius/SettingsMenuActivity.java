a9451b80e561a57ef584706af18564ace142c8a4
Timo <timojenkins21@gmail.com>
2020-06-03 12:45:47 +0200

    night mode werkt, kleuren kunnen nog een beetje worden fine getuned

--- app/src/main/java/com/example/deschatkamervankoningavanius/SettingsMenuActivity.java
+++ app/src/main/java/com/example/deschatkamervankoningavanius/SettingsMenuActivity.java
@@ -1,10 +1,15 @@
 package com.example.deschatkamervankoningavanius;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class SettingsMenuActivity extends AppCompatActivity {
 
@@ -12,6 +17,35 @@ public class SettingsMenuActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingsscreen);
+
+        SharedPreferences appSettingPrefs = this.getSharedPreferences("AppSettingPrefs", 0);
+        final SharedPreferences.Editor sharedPrefsEdit = appSettingPrefs.edit();
+        Boolean isNightModeOn = appSettingPrefs.getBoolean("NightMode", false);
+
+        Switch switchButton = findViewById(R.id.switch1);
+
+        if(isNightModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            switchButton.setChecked(true);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            switchButton.setChecked(false);
+        }
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    sharedPrefsEdit.putBoolean("NightMode", true);
+                    sharedPrefsEdit.apply();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    sharedPrefsEdit.putBoolean("NightMode", false);
+                    sharedPrefsEdit.apply();
+                }
+            }
+        });
     }
 
     public void onButtonBackClicked(View view){
